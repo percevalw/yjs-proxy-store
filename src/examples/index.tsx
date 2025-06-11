c81e338277d8c35b3945a2f9d3de14e7e139692b
Perceval Wajsbürt <perceval.wajsburt@aphp.fr>
2025-06-12 01:14:20 +0200

    working prototype

--- src/examples/index.tsx
+++ src/examples/index.tsx
@@ -1,11 +1,9 @@
 import React, { useCallback } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-
-import { proxy } from "../lib";
+import { proxy, useStore } from "../lib";
 
 import * as Y from "yjs";
-import { onKeyPress } from "../../../../../../opt/homebrew/anaconda3/envs/pret/lib/python3.8/site-packages/nbclassic/static/components/codemirror/src/edit/key_events";
 
 // State will be {
 //  "todos": [{"text": "Buy milk", "done": false}, ...]
@@ -22,7 +20,7 @@ ytodos.get(0).set("text", "Buy milk");
 ytodos.get(0).set("done", false);
 ytags.push(["shopping"]);
 
-const store = proxy(yroot);
+const storeProxy = proxy(yroot, true);
 
 const useEventCallback = (callback) => {
     const callbackRef = React.useRef(callback);
@@ -33,26 +31,31 @@ const useEventCallback = (callback) => {
     }, []);
 };
 
-const TodosList = React.memo(({ todos }) => {
+const TodoItem = React.memo(({ todo }: any) => {
+    const onToggle = useEventCallback(() => {
+        todo.done = !todo.done;
+    });
+
+    return (
+        <li>
+            <input type="checkbox" checked={todo.done} onChange={onToggle} />
+            {todo.text}
+        </li>
+    );
+});
+
+const TodosList = React.memo(() => {
+    const store = useStore(storeProxy);
     return (
         <ul>
-            {todos.map((todo, index) => (
-                <li key={index}>
-                    <input
-                        type="checkbox"
-                        checked={todo.done}
-                        onChange={() => {
-                            todo.done = !todo.done;
-                        }}
-                    />
-                    {todo.text}
-                </li>
+            {store.todos.map((todo, index) => (
+                <TodoItem key={index} todo={todo} />
             ))}
         </ul>
     );
 });
 
-const TagsList = React.memo(({ tags }) => {
+const TagsList = React.memo(({ tags }: any) => {
     return (
         <ul>
             {tags.map((tag, index) => (
@@ -64,10 +67,11 @@ const TagsList = React.memo(({ tags }) => {
 
 const App = () => {
     const [input, setInput] = React.useState("");
+    const store = useStore(storeProxy);
 
     const onClick = useEventCallback(() => {
         if (input.trim()) {
-            store.todos.push({
+            storeProxy.todos.push({
                 text: input,
                 done: false,
             });
@@ -88,14 +92,14 @@ const App = () => {
             <h1>Todo List</h1>
             <div>
                 <h2>Todos</h2>
-                <TodosList todos={store.todos} />
+                <TodosList/>
                 <input type="text" value={input} onChange={onChange} onKeyDown={onKeyDown} placeholder="Add a new todo" />
                 <button onClick={onClick}>Add Todo</button>
             </div>
             <div>
                 <h2>Tags</h2>
                 <TagsList tags={store.tags} />
-                <button onClick={() => store.tags.push(["New Tag"])}>Add Tag</button>
+                <button onClick={() => storeProxy.tags.push("New Tag")}>Add Tag</button>
             </div>
         </div>
     );
